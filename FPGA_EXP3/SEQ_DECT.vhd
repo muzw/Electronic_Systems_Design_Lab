5c995213a76a7a016aad52e2c1afe726632846dc
ustc-mzw <2875226921@qq.com>
2022-09-27 19:03:34 +0800

    完成第二次实验序列检测器和发生器

---
+++ FPGA_EXP3/SEQ_DECT.vhd
@@ -0,0 +1,88 @@
+-- 作业1 序列检测器 @PB20051061牟真伟
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity SEQ_DECT is
+	port(
+		clk :IN std_logic;
+		reset :IN std_logic;
+		data_input :IN std_logic;
+		detector_out :OUT std_logic
+	);
+end SEQ_DECT;
+
+architecture arch_SEQ_DECT of SEQ_DECT is
+	type states is (S0,S1,S2,S3,S4,S5,S6,S7,S8,S9);
+	signal st :states;
+begin
+	detector_out <= '1' when st=S9 else '0';
+	
+
+	
+	-- 状态寄存器和次态逻辑
+	process(clk,reset)
+	begin
+		if(reset = '1') then
+			st <= S0;
+		elsif(clk'event and clk='1') then
+			case st is
+				when s0 => if(data_input = '1') then
+									st <= S1;
+								else 
+									st <= S0;
+								end if;
+				when s1 => if(data_input = '1') then
+									st <= S2;
+								else 
+									st <= S0;
+								end if;
+				when s2 => if(data_input = '1') then
+									st <= S3;
+								else 
+									st <= S0;
+								end if;
+				when s3 => if(data_input = '1') then
+									st <= S3;
+								else 
+									st <= S4;
+								end if;
+				when s4 => if(data_input = '1') then
+									st <= S5;
+								else 
+									st <= S0;
+								end if;
+				when s5 => if(data_input = '1') then
+									st <= S1;
+								else 
+									st <= S6;
+								end if;
+				when s6 => if(data_input = '1') then
+									st <= S1;
+								else 
+									st <= S7;
+								end if;
+				when s7 => if(data_input = '1') then
+									st <= S8;
+								else 
+									st <= S0;
+								end if;
+				when s8 => if(data_input = '1') then
+									st <= S9;
+								else 
+									st <= S0;
+								end if;
+				when s9 => if(data_input = '1') then
+									st <= S1;
+								else 
+									st <= S0;
+								end if;								
+			end case;
+		end if;
+	end process;
+	
+	-- 输出逻辑
+end arch_SEQ_DECT;
+	
+			
+
---
+++ FPGA_EXP3/SEQ_DECT_tb.vhd
@@ -0,0 +1,40 @@
+-- 作业1 序列检测器 test bench @PB20051061牟真伟
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity SEQ_DECT_tb is
+end SEQ_DECT_tb;
+
+architecture arch_SEQ_DECT_tb of SEQ_DECT_tb is
+	component SEQ_DECT port(
+			clk :IN std_logic;
+			reset :IN std_logic;
+			data_input :IN std_logic;
+			detector_out :OUT std_logic
+		);
+	end component;
+	signal clk :std_logic;
+	signal reset :std_logic;
+	signal data_input :std_logic;
+	signal detector_out :std_logic;
+	constant seq1 :std_logic_vector(1 to 9) := "110010011";
+begin
+	sql_dect: SEQ_DECT port map(clk, reset, data_input, detector_out);
+	reset <= '0';
+	process
+	begin
+
+
+		for i in 1 to 9 loop
+			clk <= '0';		
+			wait for 20 ns;
+			data_input <= seq1(i);
+
+			clk <= '1';
+			wait for 20 ns;
+		end loop;
+	end process;
+	
+	
+end arch_SEQ_DECT_tb;
